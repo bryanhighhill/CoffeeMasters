7d9bc332eab2c1531a5d58078ce7d1b95bbe5aeb
Bryan Highhill <bryanhighhill@gmail.com>
2023-09-01 14:36:09 -0500

    Created FirstComposable with modifiers

--- app/src/main/java/com/frontendmasters/coffeemasters/MainActivity.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/MainActivity.kt
@@ -3,13 +3,17 @@ package com.frontendmasters.coffeemasters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.frontendmasters.coffeemasters.ui.theme.CoffeeMastersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,26 +23,42 @@ class MainActivity : ComponentActivity() {
             CoffeeMastersTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Text("Hello World")
-                //                    Greeting("Android")
+                    FirstComposable()
                 }
             }
         }
     }
 }
 
+//Composable - UI component
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun FirstComposable() {
+    Text("Hello Jetpack Compose", modifier = Modifier
+        .background(Color.Yellow)
+        .padding(16.dp)
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoffeeMastersTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
