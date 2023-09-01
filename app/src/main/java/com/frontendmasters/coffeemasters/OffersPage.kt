a580adf0f9430604175f32b613210102346b02e1
Bryan Highhill <bryanhighhill@gmail.com>
2023-09-01 16:09:12 -0500

    added background image and theme colors

--- app/src/main/java/com/frontendmasters/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/OffersPage.kt
@@ -1,8 +1,10 @@
 package com.frontendmasters.coffeemasters
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -10,15 +12,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.frontendmasters.coffeemasters.ui.theme.Alternative2
 
 //type prev, then tab to complete
 @Preview(showBackground = true, widthDp = 400)
 @Composable
 fun Offer() {
+    Image(painter = painterResource(id = R.drawable.background_pattern),
+        contentDescription = "Background pattern",
+        contentScale = ContentScale.FillWidth,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(180.dp)
+        )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(16.dp)
@@ -27,14 +38,14 @@ fun Offer() {
             style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .background(Color.Cyan)
+                .background(Alternative2)
                 .padding(16.dp)
             )
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Description",
             style = MaterialTheme.typography.titleSmall,
             modifier = Modifier
-                .background(Color.Cyan)
+                .background(Alternative2)
                 .padding(16.dp)
             )
     }
