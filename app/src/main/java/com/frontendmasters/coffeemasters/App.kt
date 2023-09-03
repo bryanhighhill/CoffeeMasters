9bb55ca3744634ad5f35365bc80bff0b4a3aece0
Bryan Highhill <bryanhighhill@gmail.com>
2023-09-03 15:24:19 -0500

    created DataModel.kt which contains classes for app

--- app/src/main/java/com/frontendmasters/coffeemasters/App.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/App.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.frontendmasters.coffeemasters.pages.InfoPage
+import com.frontendmasters.coffeemasters.pages.MenuPage
 import com.frontendmasters.coffeemasters.pages.OffersPage
+import com.frontendmasters.coffeemasters.pages.OrderPage
 import com.frontendmasters.coffeemasters.ui.theme.CoffeeMastersTheme
 
 @Preview
@@ -46,10 +48,10 @@ fun App() {
         },
         content = {
             when(selectedRoute.value) {
-                Routes.MenuPage.route -> Text("Menu")
+                Routes.MenuPage.route -> MenuPage()
                 Routes.OffersPage.route -> OffersPage()
-                Routes.OrderPage.route -> Text("Order")
-                Routes.InfoPage.route -> Text("Info")
+                Routes.OrderPage.route -> OrderPage()
+                Routes.InfoPage.route -> InfoPage()
             }
         },
         bottomBar = {
