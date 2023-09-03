959691b094bc76ca2fb9e7d77b355f645e7dee2c
Bryan Highhill <bryanhighhill@gmail.com>
2023-09-03 14:05:21 -0500

    added navBarItem column layout to BottomNavigation.kt

--- app/src/main/java/com/frontendmasters/coffeemasters/BottomNavigation.kt
+++ app/src/main/java/com/frontendmasters/coffeemasters/BottomNavigation.kt
@@ -1,11 +1,25 @@
 package com.frontendmasters.coffeemasters
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.frontendmasters.coffeemasters.ui.theme.Alternative1
+import com.frontendmasters.coffeemasters.ui.theme.OnPrimary
 
 data class NavPage(var name: String, var icon: ImageVector, var route: String)
 
@@ -16,4 +30,30 @@ object Routes {
     val InfoPage = NavPage("Info", Icons.Outlined.Info, "menu")
 
     val pages = listOf(MenuPage, OffersPage, OrderPage, InfoPage)
+}
+
+@Preview
+@Composable
+fun NavBarItem_Preview() {
+    NavBarItem(page = Routes.MenuPage, modifier = Modifier.padding(8.dp))
+}
+@Composable //Your own composables do not accept modifiers. If you want that, you have to accept that in function signature
+fun NavBarItem(page: NavPage, selected: Boolean = false, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(horizontal = 12.dp)) {
+        Image(
+            imageVector = page.icon,
+            contentDescription = page.name,
+            colorFilter = ColorFilter.tint(
+                if (selected) Alternative1 else OnPrimary
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .size(24.dp)
+        )
+        Text(page.name,
+            fontSize = 12.sp,
+            color = if (selected) Alternative1 else OnPrimary
+        )
+    }
 }
